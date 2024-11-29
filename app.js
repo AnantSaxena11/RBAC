673acb746f7329acf3d3164c055be3a1f8ac7714
Anant Saxena <anantsaxena5454@gmail.com>
2024-11-29 15:50:38 +0530

    Routes Setup

--- app.js
+++ app.js
@@ -1,34 +1,123 @@
-const express = require("express")
-const createHttpError = require("http-errors")
-const morgan = require("morgan")
-const mongoose = require("mongoose")
-require('dotenv').config()
+const express = require('express');
+const createHttpError = require('http-errors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const session = require('express-session');
+const connectFlash = require('connect-flash');
+const passport = require('passport');
+const connectMongo = require('connect-mongo');
+const { ensureLoggedIn } = require('connect-ensure-login');
+const { roles } = require('./utils/constants');
 
+// Initialization
+const app = express();
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const app = express()
-app.use(morgan('dev')); // to get log statements in the console we use morgan package 
+const MongoStore = connectMongo(session);
+// Init Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      // secure: true,
+      httpOnly: true,
+    },
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
-app.get('/', (req, res, next) => {
-    res.send("Server is working")
-})
+// For Passport JS Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+require('./utils/passport.auth');
+
 app.use((req, res, next) => {
-    next(createHttpError.NotFound()) // this is the 404 ERROR HANDLER 
+  res.locals.user = req.user;
+  next();
 });
 
+// Connect Flash
+app.use(connectFlash());
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.route'));
+app.use('/auth', require('./routes/auth.route'));
+app.use(
+  '/user',
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
+  require('./routes/user.route')
+);
+app.use(
+  '/admin',
+  ensureLoggedIn({ redirectTo: '/auth/login' }),
+  ensureAdmin,
+  require('./routes/admin.route')
+);
+
+// 404 Handler
+app.use((req, res, next) => {
+  next(createHttpError.NotFound());
+});
+
+// Error Handler
 app.use((error, req, res, next) => {
-    error.status = error.status || 500
-    res.status(error.status)
-    res.send(error)
-})
-const PORT = process.env.PORT || 3000
+  error.status = error.status || 500;
+  res.status(error.status);
+  res.render('error_40x', { error });
+});
 
+// Setting the PORT
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    dbName: process.env.DB_NAME
-}).then(() =>{
-    console.log("💾 connected")
-    app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`))
-}).catch((err) => {1
-    console.log(err)
-})
-// its a promise so it will take time to return the response hence if the database is not connected then there is no point in waiting and connecting to the port as we will not be able to serve the client 
+// Making a connection to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('💾 connected...');
+    // Listening for connections on the defined PORT
+    app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+  })
+  .catch((err) => console.log(err.message));
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.redirect('/auth/login');
+//   }
+// }
+
+function ensureAdmin(req, res, next) {
+  if (req.user.role === roles.admin) {
+    next();
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route');
+    res.redirect('/');
+  }
+}
+
+function ensureModerator(req, res, next) {
+  if (req.user.role === roles.moderator) {
+    next();
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route');
+    res.redirect('/');
+  }
+}
